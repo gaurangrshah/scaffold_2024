c0e3a8d003acd1f1826cc15fd2596463a538c901
Gaurang Shah <gaurang.r.shah@gmail.com>
2024-03-12 00:07:47 -0400

    add consent options key map for data

--- lib/google-tag-manager/utils.ts
+++ lib/google-tag-manager/utils.ts
@@ -16,6 +16,37 @@ export const adCookies = [
   "ad_user_data",
 ];
 
+export const consentOptions = {
+  security_storage: {
+    label: "Security Related Cookies",
+    description: "Cookies necessary for securely authenticating users.",
+  },
+  functionality_storage: {
+    label: "Functionality Related Cookies",
+    description: "Cookies for measuring and improving site performance.",
+  },
+  personalization_storage: {
+    label: "Personalization Related Cookies",
+    description: "Cookies for enhanced functionality and personalization.",
+  },
+  ad_storage: {
+    label: "Marketing and Personalized Content Related Cookies",
+    description: "Cookies for targeted content delivery based on interests.",
+  },
+  analytics_storage: {
+    label: "Analytics Related Cookies",
+    description: "Cookies for measuring and improving site performance.",
+  },
+  ad_personalization: {
+    label: "Personalization Related Cookies",
+    description: "Cookies for enhanced functionality and personalization.",
+  },
+  ad_user_data: {
+    label: "User Data Related Cookies",
+    description: "Cookies for targeted content delivery based on interests.",
+  },
+};
+
 export const redactionCookie = "ads_data_redaction";
 
 export const cookieExpiry = 60 * 60 * 24 * 7; // Set expiration (1 week)
