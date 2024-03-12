a3d0db8033282aa0bf8c0e1b3619a4e1893511f6
Gaurang Shah <gaurang.r.shah@gmail.com>
2024-03-11 22:41:47 -0400

    fix: dynamically import theme-provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+// import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import dynamic from "next/dynamic";
 
-export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((mod) => mod.ThemeProvider),
+  {
+    ssr: false,
+  }
+);
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
