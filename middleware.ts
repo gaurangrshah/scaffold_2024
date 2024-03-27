829b2fb91da32fc307a10016a6e5565e9b54a7f6
Gaurang Shah <gaurang.r.shah@gmail.com>
2024-03-26 23:24:12 -0400

    chore: add all api routes to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,22 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({ ignoredRoutes: ["/"] });
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/clerk-webhook",
+    "/api/drive-activity/notification",
+    "/api/payment/success",
+  ],
+  ignoredRoutes: [
+    "/api/auth/callback/discord",
+    "/api/auth/callback/notion",
+    "/api/auth/callback/slack",
+    "/api/flow",
+    "/api/cron/wait",
+  ],
+});
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
