4fce25ae25b11ff5281b3c4e7644ad46bcbe14ab
Gaurang Shah <gaurang.r.shah@gmail.com>
2024-03-26 23:23:53 -0400

    feat: complete dash/prefs

--- app/(main)/(pages)/preferences/page.tsx
+++ app/(main)/(pages)/preferences/page.tsx
@@ -1,55 +1,55 @@
 import { ProfileForm } from "@/components/forms/profile-form";
 import { ProfilePicture } from "./_components/profile-picture";
-// import { db } from '@/lib/db'
-// import { currentUser } from '@clerk/nextjs'
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs";
 
 type Props = {};
 
 export default async function PreferencesPage(props: Props) {
-  // const authUser = await currentUser()
-  // if (!authUser) return null
+  const authUser = await currentUser();
+  if (!authUser) return null;
 
-  // const user = await db.user.findUnique({ where: { clerkId: authUser.id } })
-  // const removeProfileImage = async () => {
-  //   'use server'
-  //   const response = await db.user.update({
-  //     where: {
-  //       clerkId: authUser.id,
-  //     },
-  //     data: {
-  //       profileImage: '',
-  //     },
-  //   })
-  //   return response
-  // }
+  const user = await db.user.findUnique({ where: { clerkId: authUser.id } });
+  const removeProfileImage = async () => {
+    "use server";
+    const response = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        profileImage: "",
+      },
+    });
+    return response;
+  };
 
-  // const uploadProfileImage = async (image: string) => {
-  //   'use server'
-  //   const id = authUser.id
-  //   const response = await db.user.update({
-  //     where: {
-  //       clerkId: id,
-  //     },
-  //     data: {
-  //       profileImage: image,
-  //     },
-  //   })
+  const uploadProfileImage = async (image: string) => {
+    "use server";
+    const id = authUser.id;
+    const response = await db.user.update({
+      where: {
+        clerkId: id,
+      },
+      data: {
+        profileImage: image,
+      },
+    });
 
-  //   return response
-  // }
+    return response;
+  };
 
   const updateUserInfo = async (name: string) => {
     "use server";
 
-    // const updateUser = await db.user.update({
-    //   where: {
-    //     clerkId: authUser.id,
-    //   },
-    //   data: {
-    //     name,
-    //   },
-    // })
-    // return updateUser
+    const updateUser = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        name,
+      },
+    });
+    return updateUser;
   };
 
   return (
@@ -61,14 +61,11 @@ export default async function PreferencesPage(props: Props) {
             Add or update your information
           </p>
         </div>
-        {/* <ProfilePicture
-          // onDelete={removeProfileImage}
-          onDelete={() => {}}
-          // userImage={user?.profileImage || ''}
-          userImage={''}
-          // onUpload={uploadProfileImage}
-          onUpload={() => {}}
-        /> */}
+        <ProfilePicture
+          onDelete={removeProfileImage}
+          userImage={user?.profileImage || ""}
+          onUpload={uploadProfileImage}
+        />
         <ProfileForm user={{}} onUpdate={updateUserInfo} />
       </div>
     </>
