a69596253b6aa1e8942e884e4a149c8fa9296a94
Gaurang Shah <gaurang.r.shah@gmail.com>
2024-04-06 22:29:06 -0400

    feat: add mdx support + custom mdx comp

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,5 +1,5 @@
 import { posts } from "#site/content";
-// import { MDXContent } from "@/components/mdx-components"; // @FIXME: WIP imports
+import { MDXContent } from "@/components/mdx-components";
 import { notFound } from "next/navigation";
 
 // import "@/styles/mdx.css";
@@ -86,7 +86,7 @@ export default async function PostPage({ params }: PostPageProps) {
         <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
       ) : null}
       <hr className="my-4" />
-      {/* <MDXContent code={post.body} /> */}
+      <MDXContent code={post.body} />
     </article>
   );
 }
